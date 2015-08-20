6f6359491cbc5f1d59439e902dd22719d59b762d
Clare <creative@v1020-wn-106-208.campus-dynamic.uwaterloo.ca>
2015-08-20 14:52:38 -0400

    Updating files to work again, added gradient bg

--- projects/projects.js
+++ projects/projects.js
@@ -10,5 +10,26 @@ angular.module('myApp.projects', ['ngRoute'])
 }])
 
 .controller('ProjectsCtrl', [function() {
-
+	$scope.projects = [
+	{
+		name: 'Math Orientation 2015 Website',
+		desc: 'Heavily involved with the design of the site.',
+		tools: 'Photoshop, Illustrator'
+	},
+	{
+		name: 'Personal website',
+		desc: 'A project to learn some AngularJS.',
+		tools: 'HTML5, CSS3, Sass, AngularJS'
+	},
+	{
+		name: 'Women in Computer Science Website [WIP]',
+		desc: 'Co-designed and implemented a new design for the club site.',
+		tools: 'HTML5, CSS3, Sass, Pelican (Python framework)'
+	},
+	{
+		name: 'Love Letter Card Game',
+		desc: 'Made a C++ version of <a href="LINK TO GAME SITE HERE">this fun little card game</a> for practicing OOP skills.',
+		tools: 'C++'
+	}
+	];
 }]);
